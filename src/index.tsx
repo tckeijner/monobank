e5d1a252f8af6a3c95d972352157d7376845237b
tckeijner <tckeijner@gmail.com>
2022-06-25 11:40:04 +0200

    install typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store/index';
-// import index from './redux/index';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+)
+
+root.render(
     <Provider store={store}>
         <App />
     </Provider>, 
-    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
